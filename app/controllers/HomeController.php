7600cb13d8a4188863e63fa80bfa15826d369c48
marcdevi <rikemarcdevy.ngbehin@hetic.net>
2022-03-29 13:42:54 +0200

    com

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,2 +1,3 @@
 <?php
 
+$res = 'SELECT edition.editeur, plateforme.nom as plateforme, edition.pegi, support.nom as support FROM edition RIGHT JOIN plateforme ON edition.platforme_id = plateforme.id RIGHT JOIN support ON edition.support_id = support.id';
